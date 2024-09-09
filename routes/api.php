e01c0ad9d441416c4a8f53c90464997d9cb0340d
rajendranewput <rajendra@newput.com>
2024-09-09 13:49:32 +0530

    CORS issue fixing

--- routes/api.php
+++ routes/api.php
@@ -7,20 +7,18 @@ use App\Http\Controllers\PurchasingController;
 use App\Http\Controllers\CorController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\FarmtoforkController;
-use App\Http\Controllers\CookedLeakageController;
 
-Route::get('/user', function (Request $request) {
-    echo 'hello';
-    //return $request->user();
+
+Route::middleware(['cors'])->group(function () {
+
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    Route::post('/farm-fork-spend-data', [PurchasingController::class, 'farmForkSpendData']);
+    Route::post('/cooked-leakage-data', [PurchasingController::class, 'purchasingCookedLeakageData']);
+    Route::post('/farm-to-fork-gl-code-graph', [FarmtoforkController::class, 'farmToForkGLCodeData']);
+    Route::post('/cor-data', [CorController::class, 'CorData']);
+    Route::post('/set-costcenter', [LoginController::class, 'setCostCentersToRedis']);
 });
-
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{id}', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
-Route::post('/farm-fork-spend-data', [FarmtoforkController::class, 'farmForkSpendData']);
-Route::post('/cooked-leakage-data', [CookedLeakageController::class, 'cookedLeakageData']);
-Route::post('/farm-to-fork-gl-code-graph', [FarmtoforkController::class, 'farmToForkGLCodeData']);
-Route::post('/cor-data', [CorController::class, 'CorData']);
-Route::post('/set-costcenter', [LoginController::class, 'setCostCentersToRedis']);
