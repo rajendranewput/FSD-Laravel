5c3e7417d0a758ec455c12e3ec3c831dbd911028
rajendranewput <rajendra@newput.com>
2024-09-25 10:56:40 +0530

    Flavor first report

--- app/Http/Controllers/FlavorFirstController.php
+++ app/Http/Controllers/FlavorFirstController.php
@@ -19,11 +19,11 @@ class FlavorFirstController extends Controller
     public function export(Request $request){
         ini_set('memory_limit', '1024M');
         try{
-            $year = '2024';
-            $campusFlag = 10;
-            $date = array('2024-02-01');
-            $type = 'summary';
-            $team_name = 'A00000';
+            $year = $request->year;
+            $campusFlag = $request->campus_flag;
+            $date = $this->handleDates($request->end_date, $request->campus_flag);
+            $type = $request->type;
+            $team_name = $request->team_name;
             if($type == 'campus'){
                 $costCenter = json_decode(Redis::get('cost_campus'.$team_name), true);
             } else {
@@ -32,11 +32,11 @@ class FlavorFirstController extends Controller
             $filePath = 'flavor_first_report.xlsx';
             $url = Excel::store(new MultiSheetExport($year, $campusFlag, $date, $costCenter), $filePath, 'public');
             $url = Storage::disk('public')->url($filePath);
-            echo $url;
-            die;
-            // Return the URL
-            return response()->json(['url' => $url], 200);
-            //return Excel::download(new MultiSheetExport($year, $campusFlag, $date, $costCenter), 'multi_sheet_export.xlsx');
+            
+            return response()->json([
+                'status' => 'success',
+                'data' => $url,
+            ], 200);
         } catch(\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
