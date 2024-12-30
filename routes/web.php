7572f0f3ae3209c4ad4a3e2e4e8377988f0f02da
rajendranewput <rajendra@newput.com>
2024-12-30 15:02:27 +0530

    testing

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-   return view('welcome');
+    echo 'hello';
+   //return view('welcome');
 });
 
 Route::get('user', function () {
